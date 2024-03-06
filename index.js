e631154b4dd0e865c4b1bbf50666ef37f6145f6c
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-06 10:28:51 -0300

    agregamos una ruta en productos.routes y su respectivo controlador

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'; //permite procesar variables de entorno
 import morgan from 'morgan';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import path from 'path';
+import productosRouter from './src/routes/productos.routes.js'
 
 //1- configurar un puerto
 //puedo compilar el index.js usando node --watch index.js en forma experimental
 
 const app = express();
 //crear una variable
-app.set('port',process.env.PORT || 4000)
-app.listen(app.get('port'), ()=>{
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), () => {
     console.log('Estoy en el puerto ' + app.get('port'))
 })
 
@@ -19,16 +20,20 @@ app.listen(app.get('port'), ()=>{
 app.use(cors()) //permitir conexiones remotas
 app.use(morgan('dev')) //nos da info extra en la terminal
 app.use(express.json()) //interpretar datos en formato json
-app.use(express.urlencoded({extended: true})) //ayuda a interpretar los datos del body del request
+app.use(express.urlencoded({ extended: true })) //ayuda a interpretar los datos del body del request
 //falta configurar el index.html
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 // console.log(__filename)
 app.use(express.static(path.join(__dirname, '/public')))
 
+
 //3- configurar las rutas
-app.get('/nuevo', (req, res)=>{
-    console.log('Alguien solicito algo')
-    //falta configurar la respuesta
-    res.send('respuesta desde nuestro backend de rollingCoffee')
-})
+//hhtp://localhost:4001/api/productos
+app.use('/api', productosRouter)
+
+// app.get('/nuevo', (req, res) => {
+//     console.log('Alguien solicito algo')
+//     //falta configurar la respuesta
+//     res.send('respuesta desde nuestro backend de rollingCoffee')
+// })
