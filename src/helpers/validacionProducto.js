d15c31827d97833f8843278857d49ef997905275
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-20 11:31:19 -0300

    agregue las validaciones para el producto

--- src/helpers/validacionProducto.js
+++ src/helpers/validacionProducto.js
@@ -21,6 +21,30 @@ const validacionesProducto = [
                 throw new Error("El precio debe estar entre $100 y $2000");
             }
         }),
+        check("imagen")
+        .notEmpty()
+        .withMessage("La imagen es un dato obligatorio")
+        .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+        .withMessage("La imagen debe tener una URL válida"),
+        check("categoria")
+        .notEmpty()
+        .withMessage("La categoría es un dato obligatorio")
+        .isIn(['Infusiones', 'Batidos', 'Dulce', 'Salado'])
+        .withMessage("La categoría debe ser una de las siguientes opciones: 'Infusiones', 'Batidos', 'Dulce', 'Salado'"),
+        check("descripcionBreve")
+        .notEmpty()
+        .withMessage("La descripción breve es un dato obligatorio")
+        .isLength({ min: 10, max: 50 })
+        .withMessage(
+            "La descripción breve debe tener entre 10 y 50 caracteres"
+        ),
+        check("descripcionAmplia")
+        .notEmpty()
+        .withMessage("La descripción amplia es un dato obligatorio")
+        .isLength({ min: 100, max: 400 })
+        .withMessage(
+            "La descripción amplia debe tener entre 100 y 400 caracteres"
+        ),
     (req, res, next) => { resultadoValidacion(req, res, next) }
 ]
 
